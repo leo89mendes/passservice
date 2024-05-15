2d2e5ea3f958b4443ed7dac75615ac3d06fe960e
Leonardo Mendes <leo.89mendes@gmail.com>
2024-05-14 22:05:24 -0300

    Revert "no message"
    This reverts commit ac19966c1c8c840970272f6826b366ba19f7b92e.

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Filament\Models\Contracts\FilamentUser;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable implements FilamentUser
+class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -43,9 +42,4 @@ class User extends Authenticatable implements FilamentUser
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
-
-    public function canAccessFilament(): bool
-    {
-        return str_ends_with($this->email, '@gmail.com') && $this->hasVerifiedEmail();
-    }
 }
